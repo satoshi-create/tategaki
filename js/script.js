e913cfd6eee9b2c157d270bb5bd39fc5e0905ccf
satoshi <mimizunobouken@gmail.com>
2022-06-05 14:32:58 +0900

    add

--- js/script.js
+++ js/script.js
@@ -1,56 +1,54 @@
-
 import data from "../data/mokuji.js";
 const { mokuji, text } = data;
 
+const colors = document.querySelector(".colors");
 
-  const colors = document.querySelector(".colors");
+colors.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
 
-  colors.addEventListener("click", function (e) {
-    const id = e.target.dataset.id;
+  const element = document.documentElement;
 
-    const element = document.documentElement;
+  switch (id) {
+    case "white":
+      element.classList.remove(...element.classList);
+      document.documentElement.classList.add("white-theme");
+      break;
+    case "gray":
+      element.classList.remove(...element.classList);
+      document.documentElement.classList.add("gray-theme");
+      break;
+    case "black":
+      element.classList.remove(...element.classList);
+      document.documentElement.classList.add("dark-theme");
+      break;
+  }
+});
 
-    switch (id) {
-      case "white":
-        element.classList.remove(...element.classList);
-        document.documentElement.classList.add("white-theme");
-        break;
-      case "gray":
-        element.classList.remove(...element.classList);
-        document.documentElement.classList.add("gray-theme");
-        break;
-      case "black":
-        element.classList.remove(...element.classList);
-        document.documentElement.classList.add("dark-theme");
-        break;
-    }
-  });
+const mokujiText = document.querySelector(".translate-text");
+const container = document.querySelector(".container");
 
-  const mokujiText = document.querySelector(".translate-text");
-  const container = document.querySelector(".container");
-
-  mokujiText.innerHTML = mokuji
-    .map((item, index) => {
-      return `
+mokujiText.innerHTML = mokuji
+  .map((item, index) => {
+    return `
 <li>
   <a href="#s${index}">
     <h3>${item}</h3>
   </a>
 </li>
 `;
-    })
-    .join("");
+  })
+  .join("");
 
-  const navs = document.querySelector(".nav");
-  const navsBtn = navs.querySelector(".translate-btn.nav-btn");
-  navsBtn.addEventListener("click", function () {
-    navs.classList.toggle("show-text");
-  });
+const navs = document.querySelector(".nav");
+const navsBtn = navs.querySelector(".translate-btn.nav-btn");
+navsBtn.addEventListener("click", function () {
+  navs.classList.toggle("show-text");
+});
 
-  container.innerHTML = text
-    .map((item, index) => {
-      const { kobun, gendaibun, title, phrase } = item;
-      return `  
+container.innerHTML = text
+  .map((item, index) => {
+    const { kobun, gendaibun, title, phrase } = item;
+    return `  
 <section class="section section${index + 1}">
   <div class="kobun-text">
     ${title ? title : ""}
@@ -93,175 +91,175 @@ const { mokuji, text } = data;
      </div>
 </section>
 `;
-    })
-    .join("");
+  })
+  .join("");
 
-  const sections = document.querySelectorAll(".section");
-  sections.forEach(function (section) {
-    const btn = section.querySelector(".translate-btn.section-btn");
+const sections = document.querySelectorAll(".section");
+sections.forEach(function (section) {
+  const btn = section.querySelector(".translate-btn.section-btn");
 
-    btn.addEventListener("click", function () {
-      section.classList.add("red");
-      sections.forEach(function (item) {
-        if (item !== section) {
-          item.classList.remove("show-text");
-        }
-      });
-      section.classList.toggle("show-text");
-    });
-  });
-
-  const toggleTextBtn = document.querySelector(".toggle-text-btn");
-  const kobunTextP = document.querySelectorAll(".kobun-text p");
-  const translateTextP = document.querySelectorAll(
-    ".translate-text .gendaibun-text"
-  );
-  const toggleIcon = document.querySelector(".toggle-icon i");
-
-  toggleTextBtn.addEventListener("click", function () {
-    const result = container.classList.toggle("result");
-    toggleIcon.classList.toggle("rotate");
-    kobunTextP.forEach(function (kobun, i) {
-      if (result) {
-        kobun.innerHTML = `${text[i].gendaibun}`;
-      } else {
-        kobun.innerHTML = `${text[i].kobun}`;
+  btn.addEventListener("click", function () {
+    section.classList.add("red");
+    sections.forEach(function (item) {
+      if (item !== section) {
+        item.classList.remove("show-text");
       }
     });
-    translateTextP.forEach(function (translate, i) {
-      if (result) {
-        translate.innerHTML = `${text[i].kobun}`;
-      } else {
-        translate.innerHTML = `${text[i].gendaibun}`;
-      }
-    });
+    section.classList.toggle("show-text");
   });
+});
 
-  // ---fetch mokuji.json---
+const toggleTextBtn = document.querySelector(".toggle-text-btn");
+const kobunTextP = document.querySelectorAll(".kobun-text p");
+const translateTextP = document.querySelectorAll(
+  ".translate-text .gendaibun-text"
+);
+const toggleIcon = document.querySelector(".toggle-icon i");
 
-  // $.ajax({
-  //   url: url,
-  //   type: "GET",
-  //   dataType: "json",
-  // })
-  //   .done(function (data) {
-  //     console.log(data);
-  //     // loading.classList.add("success")
-  //     const { mokuji } = data;
-  //     const { text } = data;
-  //     console.log(text);
-  //     mokujiText.innerHTML = mokuji
-  //       .map((item, index) => {
-  //         return `
-  //     <li>
-  //         <a href="#s${index}">
-  //           <h3>${item}</h3>
-  //         </a>
-  //       </li>
-  //     `;
-  //       })
-  //       .join("");
-  //     const navs = document.querySelector(".nav");
-  //     const navsBtn = navs.querySelector(".translate-btn.nav-btn");
-  //     const clickNavsBtn = navsBtn.addEventListener("click", function () {
-  //       navs.classList.toggle("show-text");
-  //     });
-  //     container.innerHTML = text
-  //       .map((item, index) => {
-  //         const { kobun, gendaibun, title, phrase } = item;
-  //         return `
-  //       <section class="section section${index + 1}">
-  //         <div class="kobun-text">
-  //           ${title ? title : ""}
-  //           <p>
-  //             ${kobun}
-  //           </p>
-  //           <div class="translate">
-  //             <!-- question button -->
-  //             <button type="button" class="btn translate-btn section-btn">
-  //               <span class="plus-icon">
-  //                 <i class="far fa-plus-square"></i>
-  //               </span>
-  //               <span class="minus-icon">
-  //                 <i class="far fa-minus-square"></i>
-  //               </span>
-  //             </button>
-  //           </div>
-  //         </div>
-  //         <div class="translate-text">
-  //           <p class="gendaibun-text">${gendaibun}</p>
+toggleTextBtn.addEventListener("click", function () {
+  const result = container.classList.toggle("result");
+  toggleIcon.classList.toggle("rotate");
+  kobunTextP.forEach(function (kobun, i) {
+    if (result) {
+      kobun.innerHTML = `${text[i].gendaibun}`;
+    } else {
+      kobun.innerHTML = `${text[i].kobun}`;
+    }
+  });
+  translateTextP.forEach(function (translate, i) {
+    if (result) {
+      translate.innerHTML = `${text[i].kobun}`;
+    } else {
+      translate.innerHTML = `${text[i].gendaibun}`;
+    }
+  });
+});
 
-  //            ${
-  //              phrase
-  //                ? `<ul class="phrase-text">
-  //           ${phrase
-  //             .map((item, index) => {
-  //               return `
-  //                         <li>
-  //                 <h4>【${item.phrasekobun}】</h4>
-  //                 <p>${item.transphrase}</p>
-  //                </li>
-  //           `;
-  //             })
-  //             .join("")}
+// ---fetch mokuji.json---
 
-  //           </ul>`
-  //                : ""
-  //            }
+// $.ajax({
+//   url: url,
+//   type: "GET",
+//   dataType: "json",
+// })
+//   .done(function (data) {
+//     console.log(data);
+//     // loading.classList.add("success")
+//     const { mokuji } = data;
+//     const { text } = data;
+//     console.log(text);
+//     mokujiText.innerHTML = mokuji
+//       .map((item, index) => {
+//         return `
+//     <li>
+//         <a href="#s${index}">
+//           <h3>${item}</h3>
+//         </a>
+//       </li>
+//     `;
+//       })
+//       .join("");
+//     const navs = document.querySelector(".nav");
+//     const navsBtn = navs.querySelector(".translate-btn.nav-btn");
+//     const clickNavsBtn = navsBtn.addEventListener("click", function () {
+//       navs.classList.toggle("show-text");
+//     });
+//     container.innerHTML = text
+//       .map((item, index) => {
+//         const { kobun, gendaibun, title, phrase } = item;
+//         return `
+//       <section class="section section${index + 1}">
+//         <div class="kobun-text">
+//           ${title ? title : ""}
+//           <p>
+//             ${kobun}
+//           </p>
+//           <div class="translate">
+//             <!-- question button -->
+//             <button type="button" class="btn translate-btn section-btn">
+//               <span class="plus-icon">
+//                 <i class="far fa-plus-square"></i>
+//               </span>
+//               <span class="minus-icon">
+//                 <i class="far fa-minus-square"></i>
+//               </span>
+//             </button>
+//           </div>
+//         </div>
+//         <div class="translate-text">
+//           <p class="gendaibun-text">${gendaibun}</p>
 
-  //            </div>
+//            ${
+//              phrase
+//                ? `<ul class="phrase-text">
+//           ${phrase
+//             .map((item, index) => {
+//               return `
+//                         <li>
+//                 <h4>【${item.phrasekobun}】</h4>
+//                 <p>${item.transphrase}</p>
+//                </li>
+//           `;
+//             })
+//             .join("")}
 
-  //       </section>
-  //     `;
-  //       })
-  //       .join("");
+//           </ul>`
+//                : ""
+//            }
 
-  //     const sections = document.querySelectorAll(".section");
-  //     sections.forEach(function (section) {
-  //       const btn = section.querySelector(".translate-btn.section-btn");
+//            </div>
 
-  //       btn.addEventListener("click", function () {
-  //         section.classList.add("red");
-  //         sections.forEach(function (item) {
-  //           if (item !== section) {
-  //             item.classList.remove("show-text");
-  //           }
-  //         });
-  //         section.classList.toggle("show-text");
-  //       });
-  //     });
+//       </section>
+//     `;
+//       })
+//       .join("");
 
-  //     const div = document.querySelector(".div");
-  //     const btn = document.querySelector("button");
-  //     const A = document.querySelectorAll(".A");
-  //     const B = document.querySelectorAll(".B");
+//     const sections = document.querySelectorAll(".section");
+//     sections.forEach(function (section) {
+//       const btn = section.querySelector(".translate-btn.section-btn");
 
-  //     const toggleTextBtn = document.querySelector(".toggle-text-btn");
-  //     const kobunTextP = document.querySelectorAll(".kobun-text p");
-  //     const translateTextP = document.querySelectorAll(
-  //       ".translate-text .gendaibun-text"
-  //     );
-  //     const toggleIcon = document.querySelector(".toggle-icon i");
+//       btn.addEventListener("click", function () {
+//         section.classList.add("red");
+//         sections.forEach(function (item) {
+//           if (item !== section) {
+//             item.classList.remove("show-text");
+//           }
+//         });
+//         section.classList.toggle("show-text");
+//       });
+//     });
 
-  //     toggleTextBtn.addEventListener("click", function () {
-  //       const result = container.classList.toggle("result");
-  //       toggleIcon.classList.toggle("rotate");
-  //       kobunTextP.forEach(function (kobun, i) {
-  //         if (result) {
-  //           kobun.innerHTML = `${text[i].gendaibun}`;
-  //         } else {
-  //           kobun.innerHTML = `${text[i].kobun}`;
-  //         }
-  //       });
-  //       translateTextP.forEach(function (translate, i) {
-  //         if (result) {
-  //           translate.innerHTML = `${text[i].kobun}`;
-  //         } else {
-  //           translate.innerHTML = `${text[i].gendaibun}`;
-  //         }
-  //       });
-  //     });
-  //   })
-  //   .fail(function (data) {
-  //     console.log("error");
-  //   });
+//     const div = document.querySelector(".div");
+//     const btn = document.querySelector("button");
+//     const A = document.querySelectorAll(".A");
+//     const B = document.querySelectorAll(".B");
+
+//     const toggleTextBtn = document.querySelector(".toggle-text-btn");
+//     const kobunTextP = document.querySelectorAll(".kobun-text p");
+//     const translateTextP = document.querySelectorAll(
+//       ".translate-text .gendaibun-text"
+//     );
+//     const toggleIcon = document.querySelector(".toggle-icon i");
+
+//     toggleTextBtn.addEventListener("click", function () {
+//       const result = container.classList.toggle("result");
+//       toggleIcon.classList.toggle("rotate");
+//       kobunTextP.forEach(function (kobun, i) {
+//         if (result) {
+//           kobun.innerHTML = `${text[i].gendaibun}`;
+//         } else {
+//           kobun.innerHTML = `${text[i].kobun}`;
+//         }
+//       });
+//       translateTextP.forEach(function (translate, i) {
+//         if (result) {
+//           translate.innerHTML = `${text[i].kobun}`;
+//         } else {
+//           translate.innerHTML = `${text[i].gendaibun}`;
+//         }
+//       });
+//     });
+//   })
+//   .fail(function (data) {
+//     console.log("error");
+//   });
